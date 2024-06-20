3b8cf6039e8010105bf6d0f933e35299f2cfb0a4
QIAO XUANYUAN <xuanyuanqiao001@gmail.com>
2024-06-20 16:46:51 -0500

    commit

--- src/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-	name='pySTIM',
-	version='1.0.4',
-	description='A python package for spatial omics analysis',
-	url='https://bitbucket.org/stim-spatial-omics/stim', 
-)
